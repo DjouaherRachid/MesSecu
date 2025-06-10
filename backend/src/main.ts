acc3604343ab7e8539eadaab87a402a9b60aaca1
Rachid <djouaherrachid@gmail.com>
2025-06-10 16:38:05 +0200

    add swagger and dependencies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuration de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Secure Messaging')
+    .setDescription('Documentation API pour la messagerie sécurisée')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
