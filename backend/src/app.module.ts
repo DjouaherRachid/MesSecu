c18b706ab0585f6eef96fb1fbaf933bb4d134f6d
Rachid <djouaherrachid@gmail.com>
2025-06-10 12:00:58 +0200

    fix backend launch

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,25 @@
+import { Message } from './message/message.entity';
 import { Conversation } from './conversation/conversation.entity';
-// import { Message } from './node_modules/typescript/lib/typescript.d';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { ConversationParticipantModule } from './conversation/conversation-participant.module';
 import { MessageModule } from './message/message.module';
+import { User } from './user/user.entity';
+import { ConversationParticipant } from './conversation/conversation-participant.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      // ta config typeorm ici (host, port, username, password, database, entities, etc)
-      autoLoadEntities: true,
-      synchronize: true, // à utiliser avec précaution en prod
+      type: process.env.DB_TYPE as 'mysql' | 'postgres' | 'sqlite',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User, Message, Conversation, ConversationParticipant],
+      synchronize: true,
     }),
     UserModule,
     ConversationModule,
